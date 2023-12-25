4829c4b9dc8ec7c02b956805382f9a2d4383d963
Gustavo William Gonçalves Silva <gustavowilliamgs1@gmail.com>
2023-12-25 23:49:28 +0000

    update

--- js/game.js
+++ js/game.js
@@ -1,39 +1,84 @@
-const $game = document.querySelector('#game');
 
-const $gameBoard = document.createElement('div');
-$gameBoard.id = 'game-board';
+(() => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    
+    let x = true;
+    let o = false;
 
-const $player1 = document.createElement('div');
-$player1.id = 'player-1';
-$player1.className = 'players';
-$player1.innerHTML = `<h2>Jogador 1</h2>`;
+    const $game = document.querySelector('#game');
 
-const $player2 = document.createElement('div');
-$player2.id = 'player-2';
-$player2.className = 'players';
-$player2.innerHTML = `<h2>Jogador 2</h2>`;
+    const $gameBoard = document.createElement('div');
+    $gameBoard.id = 'game-board';
 
-$game.appendChild($player1);
-$game.appendChild($gameBoard);
-$game.appendChild($player2);
+    const $player1 = document.createElement('div');
+    $player1.id = 'player-1';
+    $player1.className = 'players';
+    $player1.innerHTML = `<h2>Jogador 1</h2>`;
 
-for (let i = 0; i < 3; i++) {
-    const $x = document.createElement('div');
-    const $o = document.createElement('div');
+    const $player2 = document.createElement('div');
+    $player2.id = 'player-2';
+    $player2.className = 'players';
+    $player2.innerHTML = `<h2>Jogador 2</h2>`;
 
-    $x.className = 'x-parts';
-    $o.className = 'y-parts';
+    $game.appendChild($player1);
+    $game.appendChild($gameBoard);
+    $game.appendChild($player2);
 
-    $x.draggable = "true";
-    $o.draggable = "true";
+    for (let i = 0; i < 3; i++) {
+        const $x = document.createElement('div');
+        const $o = document.createElement('div');
 
-    $player1.appendChild($x);
-    $player2.appendChild($o);
-}
+        $x.className = 'x-parts parts';
+        $o.className = 'y-parts parts';
 
-for (let i = 0; i < 9; i++) {
-    const $square = document.createElement('div');
-    $square.className = 'squares';
+        $x.id = 'x-' + i;
+        $o.id = 'o-' + i;
 
-    $gameBoard.appendChild($square);
-}
+        $x.draggable = x;
+        $o.draggable = o;
+
+        $player1.appendChild($x);
+        $player2.appendChild($o);
+    }
+
+    for (let i = 0; i < 9; i++) {
+        const $square = document.createElement('div');
+        $square.className = 'squares';
+
+        $gameBoard.appendChild($square);
+    }
+
+    const $parts = document.querySelectorAll('.parts');
+    const $squares = document.querySelectorAll('.squares');
+
+    $parts.forEach(part => {
+        part.ondragstart = function(event) {
+            event.dataTransfer.setData("text", event.target.id);
+            event.dataTransfer.effectAllowed = "move";
+        };
+    });
+
+    $squares.forEach(square => {
+        square.ondragover = function(event) {
+            event.preventDefault();
+        };
+    });
+
+    $squares.forEach(square => {
+        square.ondrop = function(event) {
+            let data = event.dataTransfer.getData("text");
+            event.target.appendChild(document.getElementById(data));
+            event.preventDefault();
+            changePlayer();
+        }; 
+    });
+
+    function changePlayer() {
+        const $parts = document.querySelectorAll('.parts');
+
+        $parts.forEach(part => {
+            part.draggable = !part.draggable;
+        });
+    }
+})();
